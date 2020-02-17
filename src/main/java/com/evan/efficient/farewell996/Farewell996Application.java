7ee849678700a24be7e20f33504a7bedb57fc483
wangming2674 <wangmingmis@163.com>
2020-02-17 15:53:50 +0800

    init commit,build an init project

---
+++ src/main/java/com/evan/efficient/farewell996/Farewell996Application.java
@@ -0,0 +1,16 @@
+package com.evan.efficient.farewell996;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author evanwang
+ */
+@SpringBootApplication
+public class Farewell996Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Farewell996Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/evan/efficient/farewell996/Farewell996ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evan.efficient.farewell996;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Farewell996ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
