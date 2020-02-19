f15f4e5d4b67b30d879e7bddf792126983cec286
wangming2674 <wangmingmis@163.com>
2020-02-19 18:20:33 +0800

    test:方法引用

--- src/main/java/com/evan/efficient/farewell996/lamda/file/FileConsumer.java
+++ src/main/java/com/evan/efficient/farewell996/lamda/file/FileConsumer.java
@@ -8,7 +8,7 @@ public interface FileConsumer {
 
     /**
      * 函数式接口抽象方法
-     * @param fileContent - 文件内容字符串
+     * @param fileContent 文件内容字符串
      */
     void fileHandler(String fileContent);
 
---
+++ src/test/java/com/evan/efficient/farewell996/lambda/file/FileServiceTest.java
@@ -0,0 +1,20 @@
+package com.evan.efficient.farewell996.lambda.file;
+
+import com.evan.efficient.farewell996.lamda.file.FileService;
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class FileServiceTest {
+
+    @Test
+    public void fileHandle() throws IOException {
+        FileService fileService = new FileService();
+
+        // TODO 此处替换为本地文件的地址全路径
+        String filePath = "";
+
+        // 通过lambda表达式，打印文件内容
+        fileService.fileHandle(filePath, System.out::println);
+    }
+}
